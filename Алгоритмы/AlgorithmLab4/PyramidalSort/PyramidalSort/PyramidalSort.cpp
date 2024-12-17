3685d3953a25eefc18dd6456709f31d7e16da908
valeria <golovatova.vm@gmail.com>
2024-12-17 22:11:10 +0700

    Переделан main. Запись идет в файлы

--- Алгоритмы/AlgorithmLab4/PyramidalSort/PyramidalSort/PyramidalSort.cpp
+++ Алгоритмы/AlgorithmLab4/PyramidalSort/PyramidalSort/PyramidalSort.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <ctime> 
 #include <chrono> 
+#include <fstream>
 
 void RandMas(int* mas, int n, int a, int b)
 {
@@ -11,7 +12,7 @@ void RandMas(int* mas, int n, int a, int b)
     }
 }
 
-void Shift2(int* mas, int n, int i) {
+void Sift(int* mas, int n, int i) {
     int j = 2 * i + 1;
     int x = mas[i];
     while (j < n) {
@@ -33,10 +34,10 @@ void Shift2(int* mas, int n, int i) {
 void pyramidalSort(int* mas, int n)
 {
     for (int i = n / 2 - 1; i >= 0; i--)
-        Shift2(mas, n, i);
+        Sift(mas, n, i);
     for (int i = n - 1; i >= 1; i--) {
         std::swap(mas[0], mas[i]);
-        Shift2(mas, i, 0);
+        Sift(mas, i, 0);
     }
 }
 
@@ -53,53 +54,79 @@ bool Sort(int* mas, int n)
 int main() {
     srand(time(0));
 
-    int n = 10; 
-    int a = -10; 
-    int b = 10;
+    int size1 = 10;
+    int size2 = 20;
+    int size3 = 30;
+    int* arr1 = new int[size1];
+    int* arr2 = new int[size2];
+    int* arr3 = new int[size3];
 
-    int* mas = new int[n];  
-    RandMas(mas, n, a, b); 
+    RandMas(arr1, size1, -100, 100);
+    RandMas(arr2, size2, -50, 100);
+    RandMas(arr3, size3, -10, 100);
 
-    std::cout << "Array before sorting: ";
-    for (int i = 0; i < n; i++) {
-        std::cout << mas[i] << " ";
+    std::ofstream file1("array1.txt");
+    std::ofstream file2("array2.txt");
+    std::ofstream file3("array3.txt");
+
+    for (int i = 0; i < size1; i++) {
+        file1 << arr1[i] << " ";
+    }
+    for (int i = 0; i < size2; i++) {
+        file2 << arr2[i] << " ";
+    }
+    for (int i = 0; i < size3; i++) {
+        file3 << arr3[i] << " ";
+    }
+
+    file1.close();
+    file2.close();
+    file3.close();
+
+    pyramidalSort(arr1, size1);
+    pyramidalSort(arr2, size2);
+    pyramidalSort(arr3, size3);
+
+    std::ofstream file1_sorted("array1_sorted.txt");
+    std::ofstream file2_sorted("array2_sorted.txt");
+    std::ofstream file3_sorted("array3_sorted.txt");
+
+    for (int i = 0; i < size1; i++) {
+        file1_sorted << arr1[i] << " ";
+    }
+    for (int i = 0; i < size2; i++) {
+        file2_sorted << arr2[i] << " ";
+    }
+    for (int i = 0; i < size3; i++) {
+        file3_sorted << arr3[i] << " ";
+    }
+
+    file1_sorted.close();
+    file2_sorted.close();
+    file3_sorted.close();
+
+    std::cout << "Sorted array 1: ";
+    for (int i = 0; i < size1; i++) {
+        std::cout << arr1[i] << " ";
     }
     std::cout << std::endl;
 
-    std::cout << "Step 1: building pyramid " << std::endl;
-    for (int i = n / 2 - 1; i >= 0; i--) {
-        Shift2(mas, n, i);
-        std::cout << "Array after element sifting " << i << ": ";
-        for (int j = 0; j < n; j++) {
-            std::cout << mas[j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << "Step 2: Sorting the pyramid" << std::endl;
-    for (int i = n - 1; i >= 1; i--) {
-        std::swap(mas[0], mas[i]);
-        std::cout << "Array after exchange of elements " << 0 << " and " << i << ": ";
-        for (int j = 0; j < n; j++) {
-            std::cout << mas[j] << " ";
-        }
-        std::cout << std::endl;
-
-        Shift2(mas, i, 0); 
-        std::cout << "Array after element sifting " << 0 << ": ";
-        for (int j = 0; j < n; j++) {
-            std::cout << mas[j] << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        std::cout << mas[i] << " ";
+    std::cout << "Sorted array 2: ";
+    for (int i = 0; i < size2; i++) {
+        std::cout << arr2[i] << " ";
     }
     std::cout << std::endl;
 
-    delete[] mas; 
+    std::cout << "Sorted array 3: ";
+    for (int i = 0; i < size3; i++) {
+        std::cout << arr3[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] arr1;
+    delete[] arr2;
+    delete[] arr3;
+
     return 0;
 }
 
