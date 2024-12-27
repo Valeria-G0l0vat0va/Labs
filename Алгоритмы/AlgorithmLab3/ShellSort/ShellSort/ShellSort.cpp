f7f15f1d6eca5f796f32536b5f6473ebb1c5b3cd
valeria <golovatova.vm@gmail.com>
2024-12-28 00:24:50 +0700

    Исправлена сортировка Шелла

--- Алгоритмы/AlgorithmLab3/ShellSort/ShellSort/ShellSort.cpp
+++ Алгоритмы/AlgorithmLab3/ShellSort/ShellSort/ShellSort.cpp
@@ -1,8 +1,11 @@
-﻿#include<stdlib.h>
-#include <iostream>
+﻿#include <iostream>
+#include <cstdlib>
 #include <ctime>
+#include <chrono>
+#include <fstream>
+#include <vector>
 #include <cmath>
-#include<Windows.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -25,9 +28,9 @@ bool Sort(int* mas, int n)
 }
 
 void shellSort(int* a, int n, int* h, int t) {
-    for (int i = t - 1; i >= 0; i--) { 
+    for (int i = t - 1; i >= 0; i--) {
         int s = h[i];
-        for (int j = s; j < n; j++) { 
+        for (int j = s; j < n; j++) {
             int temp = a[j];
             int k = j;
             while (k >= s && a[k - s] > temp) {
@@ -43,6 +46,13 @@ void shellIncrements(int* h, int n, int& t) {
     t = 0;
     for (int gap = n / 2; gap > 0; gap /= 2) {
         h[t++] = gap;
+    }
+}
+
+void reverseArray(int* mas, int n)
+{
+    for (int i = 0; i < n / 2; i++) { 
+        swap(mas[i], mas[n - i - 1]);
     }
 }
 
@@ -63,38 +73,57 @@ void knuthIncrements(int* h, int n, int& t) {
     }
 }
 
-int main(){
+int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     srand(time(0));
+
     int sizes[] = { 10000, 100000, 1000000 };
-    int ranges[][2] = { {-10,10}, {-1000,1000}, {-100000,100000} };
-    int incrementAlgorithms[] = { 0, 1, 2 };
-    for (int sizeIndex = 0; sizeIndex < 3; sizeIndex++)
-    {
-        for (int rangeIndex = 0; rangeIndex < 3; rangeIndex++)
-        {
-            for (int algorithmIndex = 0; algorithmIndex < 3; algorithmIndex++)
-            {
+    int ranges[][2] = { {-10, 10}, {-1000, 1000}, {-100000, 100000} };
+    int incrementAlgorithms[] = { 0, 1, 2 }; 
+
+    vector<int*> allArrays;
+
+    for (int sizeIndex = 0; sizeIndex < 3; sizeIndex++) {
+        for (int rangeIndex = 0; rangeIndex < 3; rangeIndex++) {
+            int n = sizes[sizeIndex];
+            int a = ranges[rangeIndex][0];
+            int b = ranges[rangeIndex][1];
+
+            int* mas = new int[n];
+            RandMas(mas, n, a, b);
+            reverseArray(mas, n);
+            allArrays.push_back(mas);
+
+            ofstream file("array_" + to_string(sizeIndex) + "_" + to_string(rangeIndex) + ".txt");
+            for (int i = 0; i < n; i++) {
+                file << mas[i] << " ";
+            }
+            file.close();
+        }
+    }
+    for (int sizeIndex = 0; sizeIndex < 3; sizeIndex++) {
+        for (int rangeIndex = 0; rangeIndex < 3; rangeIndex++) {
+            for (int algorithmIndex = 0; algorithmIndex < 3; algorithmIndex++) {
                 int n = sizes[sizeIndex];
                 int a = ranges[rangeIndex][0];
                 int b = ranges[rangeIndex][1];
                 int incrementAlgorithm = incrementAlgorithms[algorithmIndex];
+
                 int maxIncrements = n / 2;
                 int* h = new int[maxIncrements];
-
                 int t;
+
                 if (incrementAlgorithm == 0) {
                     shellIncrements(h, n, t);
                 }
                 else if (incrementAlgorithm == 1) {
                     powerOfTwoMinusOneIncrements(h, n, t);
                 }
-                else
+                else {
                     knuthIncrements(h, n, t);
+                }
 
-                int* mas = new int[n];
-                RandMas(mas, n, a, b);
 
                 cout << "Размер массива: " << n << endl;
                 cout << "Диапазон значений: [" << a << ", " << b << "]" << endl;
@@ -113,23 +142,37 @@ int main(){
 
                 double totalTime = 0;
                 for (int l = 0; l < 3; l++) {
+                    int* mas = new int[n];
+
+                    int arrayIndex = sizeIndex * 3 + rangeIndex;
+
+
+                    for (int k = 0; k < n; k++) {
+                        mas[k] = allArrays[arrayIndex][k];
+                    }
+
                     clock_t start = clock();
-                    shellSort(mas, n, h, t);  
+                    shellSort(mas, n, h, t);
                     clock_t end = clock();
-                    double duration = double(end - start) / CLOCKS_PER_SEC; 
+                    double duration = double(end - start) / CLOCKS_PER_SEC;
                     totalTime += duration;
+
 
                     if (!Sort(mas, n)) {
                         cout << "Ошибка: массив не отсортирован" << endl;
                     }
-                    RandMas(mas, n, a, b);
+                    delete[] mas;
+
                 }
                 cout << "Среднее время работы: " << (totalTime / 3.0) * 1000000 << " микросекунд" << endl;
 
                 delete[] h;
-                delete[] mas;
             }
         }
     }
+    for (int* arr : allArrays) {
+        delete[] arr;
+    }
+
     return 0;
 }
