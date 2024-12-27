2c77e3d5c7d30f2d35dd9cb68793cad7a07fdb33
valeria <golovatova.vm@gmail.com>
2024-12-28 00:22:33 +0700

    Исправлена сортировка Хоара

--- Алгоритмы/AlgorithmLab5/QuickSort/QuickSort/QuickSort.cpp
+++ Алгоритмы/AlgorithmLab5/QuickSort/QuickSort/QuickSort.cpp
@@ -1,100 +1,97 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <ctime>
-#include <chrono> 
+#include <chrono>
+#include <vector>
+#include <algorithm>
+#include<string>
 
-
-void RandMas(int* mas, int n, int a, int b)
-{
-    for (int i = 0; i < n; i++)
-    {
-        mas[i] = rand() % (b - a + 1) + a;
+void RandMas(std::vector<int>& mas, int a, int b) {
+    for (int& element : mas) {
+        element = rand() % (b - a + 1) + a;
     }
 }
 
-void hoarSort(int* A, int l, int r) {
+void hoarSort(std::vector<int>& mas, int l, int r) {
     if (l >= r) {
         return;
     }
     int i = l;
     int j = r;
-    int x = A[(l + r) / 2];
+    int x = mas[(l + r) / 2];
     while (i <= j) {
-        while (A[i] < x) {
+        while (mas[i] < x) {
             i++;
         }
-        while (A[j] > x) {
+        while (mas[j] > x) {
             j--;
         }
         if (i <= j) {
-            std::swap(A[i], A[j]);
+            std::swap(mas[i], mas[j]);
             i++;
             j--;
         }
     }
-    hoarSort(A, l, j);
-    hoarSort(A, i, r);
+    hoarSort(mas, l, j);
+    hoarSort(mas, i, r);
 }
 
-bool Sort(int* mas, int n)
-{
-    for (int i = 0; i < n - 1; i++) {
-        if (mas[i] > mas[i + 1]) {
-            return false;
-        }
-    }
-    return true;
+bool Sort(const std::vector<int>& mas) {
+    return std::is_sorted(mas.begin(), mas.end());
 }
 
-void printArray(const int* mas, int n) {
-    for (int i = 0; i < n; i++) {
-        std::cout << mas[i] << " "; 
+void printArray(const std::vector<int>& mas) {
+    for (const int& element : mas) {
+        std::cout << element << " ";
     }
     std::cout << std::endl;
 }
+
+void saveToFile(const std::string& filename, const std::vector<int>& mas) {
+    std::ofstream file(filename);
+    for (const int& element : mas) {
+        file << element << "\n";
+    }
+    file.close();
+}
+
 int main() {
-    srand(time(0)); 
-    const int n = 1000;
-    const int a = -100;
-    const int b = 100;
-    int mas1[n], mas2[n], mas3[n];
+    srand(time(0));
 
-    RandMas(mas1, n, a, b);
-    RandMas(mas2, n, a, b);
-    RandMas(mas3, n, a, b);
-    
-    for (int k = 0; k < 3; k++) {
-        int* start_mas;
-        if (k == 0) {
-            start_mas = mas1;
-            std::cout << "Array " << k + 1 << " (original): ";
-            printArray(start_mas, n);
-        }
-        else if (k == 1) {
-            start_mas = mas2;
-            std::cout << "Array " << k + 1 << " (original): ";
-            printArray(start_mas, n);
-        }
-        else {
-            start_mas = mas3;
-            std::cout << "Array " << k + 1 << " (original): ";
-            printArray(start_mas, n);
-        }
-        clock_t start = clock();
-        hoarSort(start_mas, 0, n - 1);
-        clock_t stop = clock();
-        double duration = double(stop - start) / CLOCKS_PER_SEC;
-       
-        if (!Sort(start_mas, n)) {
-            std::cout << "Array sort error!" << std::endl;
-            return 1;
-        }
+    const int sizes[] = { 10000, 100000, 1000000 };
+    const int ranges[][2] = { {-10, 10}, {-1000, 1000}, {-100000, 100000} };
+    const int totalArrays = sizeof(sizes) / sizeof(sizes[0]) * sizeof(ranges) / sizeof(ranges[0]);
 
-        std::cout << "Array " << k + 1 << " (after sort): ";
-        printArray(start_mas, n);
+    for (int size : sizes) {
+        for (const auto& range : ranges) {
+            std::vector<int> mas(size);
+            RandMas(mas, range[0], range[1]);
 
-        std::cout << "Average running time of Hoare's algorithm for an array " << k + 1 << ": " << duration << " sec" << std::endl;
+            std::string filename = "array_" + std::to_string(size) + "_" + std::to_string(range[0]) + "_" + std::to_string(range[1]) + ".txt";
+            saveToFile(filename, mas);
+
+            double totalDuration = 0;
+            const int iterations = 3;
+            for (int i = 0; i < iterations; i++) {
+                std::vector<int> tempMas = mas; 
+                clock_t start = clock();
+                hoarSort(tempMas, 0, size - 1);
+                clock_t stop = clock();
+                double duration = double(stop - start) / CLOCKS_PER_SEC;
+                totalDuration += duration;
+
+                if (!Sort(tempMas)) {
+                    std::cout << "Array sort error!" << std::endl;
+                    return 1;
+                }
+            }
+
+            double averageDuration = totalDuration / iterations;
+            std::cout << "Average running time of Hoare's algorithm for array size " << size
+                << " with range [" << range[0] << ", " << range[1] << "]: "
+                << averageDuration << " sec" << std::endl;
+        }
     }
 
     return 0;
 }
-
